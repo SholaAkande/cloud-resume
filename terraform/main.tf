586287878f151d3137232bad8e4867477c5d6e1d
Shola Akande <ishola.akande.cloud@gmail.com>
2026-04-12 20:02:42 +0100

    Initial commit: Secure WAF and Lambda Infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,72 +7,66 @@ terraform {
   }
 }
 
+# --- Providers ---
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
 
+# --- Backend Logic (Lambda & DynamoDB) ---
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
   source_dir  = "${path.module}/lambda-code"
   output_path = "${path.module}/lambda-code/lambda_function.zip"
 }
 
-resource "aws_lambda_function" "my_func"{
-  filename        = "${path.module}/lambda-code/lambda_function.zip"
-  function_name   = "shola-resume-func"
-  role            = aws_iam_role.iam_for_lambda.arn
-  handler         = "func.lambda_handler"
-  runtime         = "python3.9"
-
-  depends_on = [ aws_iam_role_policy_attachment.lambda_logs]
+resource "aws_lambda_function" "my_func" {
+  filename      = "${path.module}/lambda-code/lambda_function.zip"
+  function_name = "shola-resume-func"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "func.lambda_handler"
+  runtime       = "python3.9"
+  depends_on    = [aws_iam_role_policy_attachment.lambda_logs]
 }
 
 resource "aws_dynamodb_table" "resume_stats" {
   name         = "shola-cloud-resume-stats-2026"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
-
   attribute {
     name = "id"
     type = "S"
   }
 }
 
+# --- IAM Roles & Policies ---
 resource "aws_iam_role" "iam_for_lambda" {
   name = "shola-resume-lambda-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Sid       = ""
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
   })
 }
 
 resource "aws_iam_policy" "dynamodb_lambda_policy" {
   name        = "shola-dynamodb-lambda-policy"
   description = "IAM policy for DynamoDB access from Lambda"
-
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "dynamodb:GetItem",
-          "dynamodb:UpdateItem",
-          "dynamodb:PutItem"
-        ]
-        Effect   = "Allow"
-        Resource = aws_dynamodb_table.resume_stats.arn
-      }
-    ]
+    Statement = [{
+      Action   = ["dynamodb:GetItem", "dynamodb:UpdateItem", "dynamodb:PutItem"]
+      Effect   = "Allow"
+      Resource = aws_dynamodb_table.resume_stats.arn
+    }]
   })
 }
 
@@ -81,38 +75,34 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = aws_iam_policy.dynamodb_lambda_policy.arn
 }
 
-# 1. Create the API Gateway Container
+# --- API Gateway ---
 resource "aws_apigatewayv2_api" "lambda_api" {
   name          = "vistor-counter-api"
   protocol_type = "HTTP"
   cors_configuration {
-    allow_origins = ["*"] # This allows the website to talk to the API from anywhere
+    allow_origins = ["*"]
     allow_methods = ["GET", "POST"]
   }
 }
 
-# 2. Create the "Route" (The URL path, e.g., /get-views)
 resource "aws_apigatewayv2_route" "counter_route" {
   api_id    = aws_apigatewayv2_api.lambda_api.id
   route_key = "GET /get-views"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
-# 3. The "Integration" (The bridge between API and Lambda)
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id           = aws_apigatewayv2_api.lambda_api.id
   integration_type = "AWS_PROXY"
   integration_uri  = aws_lambda_function.my_func.invoke_arn
 }
 
-# 4. The "Stage" (The environment, like 'production')
 resource "aws_apigatewayv2_stage" "lambda_stage" {
   api_id      = aws_apigatewayv2_api.lambda_api.id
   name        = "$default"
   auto_deploy = true
 }
 
-# 5. The Permission (Crucial: Telling Lambda it's okay for API Gateway to call it)
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -121,17 +111,15 @@ resource "aws_lambda_permission" "api_gw" {
   source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
 }
 
-# 6. Output the URL so I can find it easily
 output "base_url" {
   value = aws_apigatewayv2_stage.lambda_stage.invoke_url
 }
 
-# 1. The Website S3 Bucket
+# --- Website Frontend (S3 & CloudFront) ---
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "shola-cloud-resume-2026-site" # Must be unique!
+  bucket = "shola-cloud-resume-2026-site"
 }
 
-# 2. CloudFront Origin Access Control (The Security Guard)
 resource "aws_cloudfront_origin_access_control" "default" {
   name                              = "s3-oac"
   origin_access_control_origin_type = "s3"
@@ -139,8 +127,8 @@ resource "aws_cloudfront_origin_access_control" "default" {
   signing_protocol                  = "sigv4"
 }
 
-# 3. The CloudFront Distribution
 resource "aws_cloudfront_distribution" "s3_distribution" {
+  web_acl_id = aws_wafv2_web_acl.main.arn
   origin {
     domain_name              = aws_s3_bucket.website_bucket.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
@@ -151,24 +139,18 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   default_root_object = "index.html"
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3Origin"
-
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3Origin"
+    viewer_protocol_policy = "redirect-to-https"
     forwarded_values {
       query_string = false
-      cookies {
-        forward = "none"
-      }
+      cookies { forward = "none" }
     }
-
-    viewer_protocol_policy = "redirect-to-https"
   }
 
   restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
+    geo_restriction { restriction_type = "none" }
   }
 
   viewer_certificate {
@@ -176,7 +158,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 }
 
-# Output the CloudFront URL so it can be found
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
@@ -185,20 +166,72 @@ resource "aws_s3_bucket_policy" "cdn_oac_policy" {
   bucket = aws_s3_bucket.website_bucket.id
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = "s3:GetObject"
-        Effect   = "Allow"
-        Resource = "${aws_s3_bucket.website_bucket.arn}/*"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
-          }
-        }
+    Statement = [{
+      Action    = "s3:GetObject"
+      Effect    = "Allow"
+      Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      Principal = { Service = "cloudfront.amazonaws.com" }
+      Condition = {
+        StringEquals = { "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn }
       }
-    ]
+    }]
   })
+}
+
+# --- WAF (The Bouncer) ---
+resource "aws_wafv2_web_acl" "main" {
+  provider    = aws.us_east_1
+  name        = "cloud-resume-waf"
+  description = "Rate Limiting and Managed Rules for my resume site"
+  scope       = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  # RULE 1: IP Rate Limiting
+  rule {
+    name     = "LimitRequestsPerIP"
+    priority = 1
+    action {
+      block {}
+    }
+    statement {
+      rate_based_statement {
+        limit              = 100
+        aggregate_key_type = "IP"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "LimitRequestsPerIP"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # RULE 2: AWS Managed Common Rules
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 2
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "cloud-resume-waf"
+    sampled_requests_enabled   = true
+  }
 }
